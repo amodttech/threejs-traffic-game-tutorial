dfda76f6a6a194ae8029165849b28076d69ac394
Aaron <aaron@amodttech.com>
2021-04-03 15:15:05 -0400

    set up scene

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 import _ from 'lodash'
+import { dir } from 'node:console'
+import THREE, { AmbientLight } from 'three'
+
+
+const scene = new THREE.Scene()
+
+
+const playerCar = Car()
+scene.add(playerCar)
+
+
+// lights setup
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6)
+scene.add(ambientLight)
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.6)
+dirLight.position.set(100, -300, 400)
+scene.add(dirLight)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
     return element;
   }
   
-  document.body.appendChild(component());
+document.body.appendChild(component());
